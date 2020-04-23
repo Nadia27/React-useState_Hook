debfc42a4cf14ce8ac67268ff2849a589ef95bf6
Nadia Abby <jones.nadia.l@gmail.com>
2020-04-22 23:59:29 -0400

    Destructing Example
    es6 Destructing example

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 function App() {
-  return <div />;
+  const state = useState(5664);
+
+  function increase() {
+    // //count++;
+    // console.log(count);
+  }
+
+  return (
+    <div className="container">
+      <h1>{state[0]}</h1>
+      <button onClick={increase}>+</button>
+    </div>
+  );
 }
 
 export default App;
+
+// Destructing - destructure a complex structure
+// Objects and arrays
+
+const rgb = [46, 204, 113];
+
+// To access value in the array
+console.log(rgb[2]); // bad code
+
+// Destructure example
+// instead use name inside  []
+
+const [red, green, blue] = [231, 76, 60];
+
+// varible names will be mapped to corresponding values
+console.log(green); // 76
