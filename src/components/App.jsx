f3c1b064ece42d1e22cdc4938196689b002ce23d
Nadia Abby <jones.nadia.l@gmail.com>
2020-04-23 00:20:07 -0400

    Destructing
    Theory and example of destructing the React Hook useState()

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,17 @@
 import React, { useState } from "react";
 
 function App() {
-  const state = useState(5664);
+  //State outputs an array [initial state, =>]
+  // could use destructing to assign a name to array values
+  const [count, setCount] = useState(90);
 
   function increase() {
-    // //count++;
-    // console.log(count);
+    setCount(10);
   }
 
   return (
     <div className="container">
-      <h1>{state[0]}</h1>
+      <h1>{count}</h1>
       <button onClick={increase}>+</button>
     </div>
   );
@@ -21,15 +22,15 @@ export default App;
 // Destructing - destructure a complex structure
 // Objects and arrays
 
-const rgb = [46, 204, 113];
+// const rgb = [46, 204, 113];
 
-// To access value in the array
-console.log(rgb[2]); // bad code
+// // To access value in the array
+// console.log(rgb[2]); // bad code
 
-// Destructure example
-// instead use name inside  []
+// // Destructure example
+// // instead use name inside  []
 
-const [red, green, blue] = [231, 76, 60];
+// const [red, green, blue] = [231, 76, 60];
 
-// varible names will be mapped to corresponding values
-console.log(green); // 76
+// // varible names will be mapped to corresponding values
+// console.log(green); // 76
